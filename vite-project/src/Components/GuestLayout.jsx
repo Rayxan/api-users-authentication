7f50e24ce409bdc1a8963cac97a57f629e7f8555
Raylan Sales <raylan.sales1@cidadania.gov.br>
2024-09-03 22:21:04 -0300

    adding context provider

--- vite-project/src/Components/GuestLayout.jsx
+++ vite-project/src/Components/GuestLayout.jsx
@@ -1,6 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../context/contextprovider";
 
 export default function GuestLayout(){
+    const {token} = useStateContext();
+    if(token){
+        return <Navigate to='/'/>
+    }
     return(
         <div>
             <div>
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,9 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import { ContextProvider } from './context/contextprovider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ContextProvider>
     <RouterProvider router={router} />
+    </ContextProvider>
   </StrictMode>,
 )
