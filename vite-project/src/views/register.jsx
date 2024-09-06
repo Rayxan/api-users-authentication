b83adafa3cc8c8d9456b0e7480f699afd898f1f1
Raylan Sales <raylan.sales1@cidadania.gov.br>
2024-09-06 01:10:35 -0300

    setting up axios

--- vite-project/src/views/register.jsx
+++ vite-project/src/views/register.jsx
@@ -1,8 +1,31 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import { useStateContext } from "../context/contextprovider";
 
 export default function register(){
+
+    const nameRef = useRef(); 
+    const emailRef = useRef(); 
+    const passwordRef = useRef(); 
+
+    const {setUser, setToken} = useStateContext();
+
     const Submit = (ev) =>{
         ev.preventDefault();
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        axios.Client.post("register", payload).then(({data})=>{
+            setUser(data.user);
+            setToken(data.token);
+        }).catch(err => {
+            const response = err.response;
+            if(response && response.status == 422){
+                console.log(response.data.errors);
+            }
+        });
     }
 
     return(
@@ -12,9 +35,9 @@ export default function register(){
                     Create A New Account
                 </h1>
                 <form onSubmit={Submit}>
-                    <input type="name" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input ref={nameRef} type="name" placeholder="Name" />
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input ref={passwordRef} type="password" placeholder="Password" />
                     <button className="btn btn-block">Register</button>
                     <p className="message">
                         Already Have An Account? <Link to='/login'>Login</Link>
